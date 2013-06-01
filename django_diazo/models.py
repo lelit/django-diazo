a493f9f0b778ae873e1bd2a2acb876754b54ece8
Douwe van der Meij <vandermeij@gw20e.com>
2013-06-01 16:23:33 +0200

    added help_text

--- django_diazo/models.py
+++ django_diazo/models.py
@@ -6,8 +6,10 @@ class Theme(models.Model):
     name = models.CharField(_('name'), max_length=255, blank=True)
     rules = models.CharField(_('rules'), max_length=255, blank=True)
     prefix = models.CharField(_('prefix'), max_length=255, blank=True)
-    enabled = models.BooleanField(_('enabled'), default=False)
-    debug = models.BooleanField(_('debug'), default=False)
+    enabled = models.BooleanField(_('enabled'), default=False,
+                                  help_text=_('Enable this theme (and disable the current, if enabled).'))
+    debug = models.BooleanField(_('debug'), default=False,
+                                help_text=_('Reload theme on every request (vs. reload on changing themes).'))
 
     def __unicode__(self):
         return self.name
