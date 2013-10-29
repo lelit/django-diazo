bf63cfffd205e01cc87a0406d1b13b893f977425
Douwe van der Meij <vandermeij@gw20e.com>
2013-10-29 13:45:32 +0100

    cleaned up theme admin page, bugfix for contrib theme static urls

--- django_diazo/admin/theme.py
+++ django_diazo/admin/theme.py
@@ -1,42 +1,18 @@
 import os
 import zipfile
-from codemirror.widgets import CodeMirrorTextarea
 from django import forms
 from django.contrib import admin
-from django.forms import Widget
-from django.utils.html import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django_diazo.actions import enable_theme, enable_theme_with_debug, disable_theme
 from django_diazo.models import Theme, Rule, ThemeUserAgent
 
 
-class IFrameWidget(Widget):
-    def __init__(self, attrs=None):
-        # The 'rows' and 'cols' attributes are required for HTML correctness.
-        default_attrs = {'src': 'http://localhost:8000'}
-        if attrs:
-            default_attrs.update(attrs)
-        super(IFrameWidget, self).__init__(default_attrs)
-
-    def render(self, name, value, attrs=None):
-        if value is None:
-            value = ''
-        final_attrs = self.build_attrs(attrs, name=name)
-        return mark_safe('<iframe{0} />',
-                         forms.util.flatatt(final_attrs))
-
-
 class ThemeForm(forms.ModelForm):
     upload = forms.FileField(required=False, label=_('Zip file'),
                              help_text=_('Will be unpacked in media directory.'))
-    preview = forms.URLField(required=False, widget=IFrameWidget())
 
     class Meta:
         model = Theme
-        #exclude = ('builtin', 'url', 'path')
-
-    def __init__(self, *args, **kwargs):
-        super(ThemeForm, self).__init__(*args, **kwargs)
 
     def save(self, commit=True):
         instance = super(ThemeForm, self).save(commit)
@@ -73,22 +49,12 @@ class ThemeAdmin(admin.ModelAdmin):
         """Hook for specifying fieldsets for the different forms."""
         if not obj:
             return (
-                (None, {'fields': ('name', 'slug', 'enabled', 'debug', 'sort',)}),
-                #(None, {'fields': ('name', 'slug', 'prefix', 'rules', 'enabled', 'debug')}),
-                (_('Upload theme'), {'fields': ('upload', 'prefix',)}),
-                # (_('Preview'), {'classes': (), 'fields': ('preview',)}),
+                (None, {'fields': ('name', 'slug', 'enabled', 'debug', 'sort', 'prefix',)}),
+                (_('Upload theme'), {'fields': ('upload',)}),
             )
         elif obj.builtin:
             return (
                 (None, {'fields': ('name', 'slug', 'enabled', 'debug', 'sort', 'prefix',)}),
-                #(_('Built-in settings'), {'classes': ('collapse',), 'fields': ('path', 'url', 'builtin',)})
-                # (_('Preview'), {'classes': (), 'fields': ('preview',)}),
-            )
-        else:
-            return (
-                (None, {'fields': ('name', 'slug', 'enabled', 'debug', 'sort', 'prefix',)}),
-                #(None, {'fields': ('name', 'slug', 'prefix', 'rules', 'enabled', 'debug')}),
-                # (_('Preview'), {'classes': (), 'fields': ('preview',)}),
             )
 
     def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
