c916de48e60dfbe5533a37347903dfef2c1cea56
Douwe van der Meij <vandermeij@gw20e.com>
2014-05-14 10:25:39 +0200

    moved cms middleware to contrib, bumped to 1.1.5

--- django_diazo/middleware.py
+++ django_diazo/middleware.py
@@ -1,7 +1,4 @@
 import os
-from cms.constants import RIGHT
-from cms.toolbar.items import TemplateItem
-from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.http import HttpResponse
 from diazo.wsgi import DiazoMiddleware
@@ -13,34 +10,6 @@ from repoze.xmliter.serializer import XMLSerializer
 
 from django_diazo.settings import DOCTYPE
 from django_diazo.utils import get_active_theme, check_themes_enabled, should_transform
-
-
-class DjangoCmsDiazoMiddleware(object):
-    """
-    Django CMS 3 add-on
-    """
-
-    def process_request(self, request):
-        """
-        Add Django CMS 3 on/off switch to toolbar
-        """
-        if request.user.is_staff:
-            if 'theme_on' in request.GET and not request.session.get('django_diazo_theme_enabled', False):
-                request.session['django_diazo_theme_enabled'] = True
-            if 'theme_off' in request.GET and request.session.get('django_diazo_theme_enabled', True):
-                request.session['django_diazo_theme_enabled'] = False
-        if hasattr(request, 'toolbar'):
-            request.toolbar.add_item(
-                TemplateItem(
-                    "cms/toolbar/items/on_off.html",
-                    extra_context={
-                        'request': request,
-                        'diazo_enabled': check_themes_enabled(request),
-                    },
-                    side=RIGHT,
-                ),
-                len(request.toolbar.right_items),
-            )
 
 
 class DjangoDiazoMiddleware(object):
