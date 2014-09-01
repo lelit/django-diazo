2ed4db6c0fac9c3733ca4a1c3881a7361ccf54ac
Douwe van der Meij <vandermeij@gw20e.com>
2014-09-01 22:11:09 +0200

    refactored django cms middleware and toolbar, bumped to 1.2.1

--- django_diazo/contrib/cms/middleware.py
+++ django_diazo/contrib/cms/middleware.py
@@ -1,7 +1,3 @@
-from cms.constants import RIGHT
-from cms.toolbar.items import TemplateItem
-from django_diazo.utils import check_themes_enabled
-
 
 class DjangoCmsDiazoMiddleware(object):
     """
@@ -10,22 +6,10 @@ class DjangoCmsDiazoMiddleware(object):
 
     def process_request(self, request):
         """
-        Add Django CMS 3 on/off switch to toolbar
+        Toggle theme enabled boolean
         """
         if request.user.is_staff:
             if 'theme_on' in request.GET and not request.session.get('django_diazo_theme_enabled', False):
                 request.session['django_diazo_theme_enabled'] = True
             if 'theme_off' in request.GET and request.session.get('django_diazo_theme_enabled', True):
                 request.session['django_diazo_theme_enabled'] = False
-        if hasattr(request, 'toolbar'):
-            request.toolbar.add_item(
-                TemplateItem(
-                    "cms/toolbar/items/on_off.html",
-                    extra_context={
-                        'request': request,
-                        'diazo_enabled': check_themes_enabled(request),
-                    },
-                    side=RIGHT,
-                ),
-                len(request.toolbar.right_items),
-            )
